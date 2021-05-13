2573520d4c5f3879866fb34f584a1f17589e2157
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-05-13 14:30:41 -0400

    do not crash on missing smtp server

--- pages/api/register.js
+++ pages/api/register.js
@@ -16,14 +16,19 @@ export default async (req, res) => {
     res.status(200).json({ name, email })
 
     // and then send an email
-    const info = await emailer.sendMail({
-      from: '"Registration system" <reg@company.co>',
-      to: email,
-      subject: 'Confirmation code 1️⃣2️⃣3️⃣',
-      text: 'Your confirmation code is 654agc',
-      html: confirmationEmailHTML,
-    })
-    console.log('sent a confirmation email to %s', email)
+    try {
+      const info = await emailer.sendMail({
+        from: '"Registration system" <reg@company.co>',
+        to: email,
+        subject: 'Confirmation code 1️⃣2️⃣3️⃣',
+        text: 'Your confirmation code is 654agc',
+        html: confirmationEmailHTML,
+      })
+      console.log('sent a confirmation email to %s', email)
+    } catch (e) {
+      console.error('Problem sending an email to %s', email)
+      console.error(e)
+    }
 
     return
   }
